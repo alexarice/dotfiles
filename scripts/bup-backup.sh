1a0f81a99a948493c2f2bcbcd696729b3353d4f8
Alex Rice <alexrice999@hotmail.co.uk>
2018-12-29 11:36:30 +0000

    remove root saving

--- scripts/bup-backup.sh
+++ scripts/bup-backup.sh
@@ -6,13 +6,13 @@ if [ ! -d $BUP_DIR ]; then
     echo Cannot find backup drive
     exit
 fi
-if [ "$EUID" -ne 0 ]; then
-    echo Run as root
-    exit
-fi
-echo backing up root
-bup index --exclude /var/run/media --exclude /var/cache --exclude /var/tmp /var /srv /etc /root /usr/local /opt
-bup save -n backup_root /var /srv /etc /root /usr/local /opt
+#if [ "$EUID" -ne 0 ]; then
+#    echo Run as root
+#    exit
+#fi
+#echo backing up root
+#bup index --exclude /var/run/media --exclude /var/cache --exclude /var/tmp /var /srv /etc /root /usr/local /opt
+#bup save -n backup_root /var /srv /etc /root /usr/local /opt
 echo backing up home
 bup index /home/alex
 bup save -n backup_home /home/alex
