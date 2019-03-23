ae0e12fe309f7cb165c2faed01c4c77b3498301a
Alex Rice <alexrice999@hotmail.co.uk>
2019-03-23 19:04:00 +0000

    Add metadata to brillo

--- dotfiles/brillo/default.nix
+++ dotfiles/brillo/default.nix
@@ -1,15 +1,26 @@
 {stdenv, fetchurl, which, go-md2man}:
 
-stdenv.mkDerivation rec {
-  version = "1.4.3";
-  name = "brillo-${version}";
+let
+  v = "1.4.3";
+in
+stdenv.mkDerivation {
+  version = v;
+  name = "brillo-${v}";
   src = fetchurl {
-    url = "https://gitlab.com/cameronnemo/brillo/-/archive/v1.4.3/brillo-v1.4.3.tar.bz2";
+  url = "https://gitlab.com/cameronnemo/brillo/-/archive/v${v}/brillo-v${v}.tar.bz2";
     sha256 = "0wjpw9vn521rwpnlhvczzfzdsdq812vsyl151627qw51zadynvz1";
   };
   patches = [./brillo.patch];
   makeFlags = [ "PREFIX=$(out)" ];
   nativeBuildInputs = [go-md2man which];
-  dontBuild = true;
-  installTargets = "dist install-dist";
+  buildFlags = [ "dist" ];
+  installTargets = "install-dist";
+
+  meta = with stdenv.lib; {
+    description = "Backlight and Keyboard LED control tool";
+    homepage = https://gitlab.com/cameronnemo/brillo;
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+    maintainers = [ maintainers.alexarice ];
+  };
 }
