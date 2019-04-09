f5a8f3d6128ea1b5157d5a3a0f86d68921c82541
Alex Rice <alexrice999@hotmail.co.uk>
2019-04-09 22:10:21 +0100

    Update waybar

--- dotfiles/waybar-nix/metadata.nix
+++ dotfiles/waybar-nix/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "3257968a2844ac5d1d00769c0af45398aeebdb58";
-  sha256 = "08iwnfmzm8dxbvvaqh0mrx1l7ivd60h9k2lnam8an6pdznzxjinm";
-  revdate = "2019-03-15T08:43:02Z";
+  rev = "b17984f75e80f708afe7b45c444f3732adfc2aab";
+  sha256 = "0fkcx7zh7rrmiglxxalw507gm1l3758biwj7h6lgaa8kggbmhwd3";
+  revdate = "2019-03-27T07:15:44Z";
 }
