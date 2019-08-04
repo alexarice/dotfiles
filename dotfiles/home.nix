57fc9235cc5ad6d4a7ca5455a9382be32a7c29c3
Alex Rice <alexrice999@hotmail.co.uk>
2019-08-04 15:02:23 +0100

    Remove spacemacs

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -136,10 +136,6 @@ in
     ]);
 
     programs = {
-      spacemacs = {
-        enable = true;
-        configFile = "${dots}/spacemacs";
-      };
       git = {
         enable = true;
         userName = "Alex Rice";
