0cb42d42b0a7e861a04a94b24d9dd3d439c54fcb
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-01 10:11:39 +0100

    Add alacritty

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -136,6 +136,59 @@ in
     ]);
 
     programs = {
+      alacritty = {
+        enable = true;
+        settings = {
+          tabspaces = 2;
+          font = {
+            normal = {
+              family = "Source Code Pro for Powerline";
+              style = "Regular";
+            };
+            bold = {
+              family = "Source Code Pro for Powerline";
+              style = "Bold";
+            };
+            italic = {
+              family = "Source Code Pro for Powerline";
+              style = "Italic";
+            };
+            bold-italic = {
+              family = "Source Code Pro for Powerline";
+              style = "Bold Italic";
+            };
+            size = 10;
+          };
+          colors = {
+            primary = {
+              background = "#282a36";
+              foreground = "#f8f8f2";
+            };
+            normal = {
+              black =   "#000000";
+              red =     "#ff5555";
+              green =   "#50fa7b";
+              yellow =  "#f1fa8c";
+              blue =    "#caa9fa";
+              magenta = "#ff79c6";
+              cyan =    "#8be9fd";
+              white =   "#bfbfbf";
+            };
+            bright = {
+              black =   "#575b70";
+              red =     "#ff6e67";
+              green =   "#5af78e";
+              yellow =  "#f4f99d";
+              blue =    "#caa9fa";
+              magenta = "#ff92d0";
+              cyan =    "#9aedfe";
+              white =   "#e6e6e6";
+            };
+          };
+          background_opacity = 0.8;
+          mouse.url.launcher = "firefox";
+        };
+      };
       git = {
         enable = true;
         userName = "Alex Rice";
