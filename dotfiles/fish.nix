cbbcdfd3af2d09c8cfae988abfe8b98a9f103b90
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-11 18:09:54 +0100

    Add git aliases

--- dotfiles/fish.nix
+++ dotfiles/fish.nix
@@ -3,6 +3,13 @@
 {
   programs.fish = {
     enable = true;
+    shellAliases = {
+      gp = "git push";
+      gc = "git commit -S";
+      ga = "git add";
+      gst = "git status";
+      gr = "git rebase -S";
+    };
   };
 
   xdg.configFile."fish/functions".source = pkgs.callPackage ./fish_prompt.nix { };
