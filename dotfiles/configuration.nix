4e7b65b75fcc0c81f32e746459b3e31eb305f5d4
Alex Rice <alexrice999@hotmail.co.uk>
2019-10-30 21:42:27 +0000

    Add aptx support

--- dotfiles/configuration.nix
+++ dotfiles/configuration.nix
@@ -57,6 +57,7 @@ in
       pulseaudio = {
         enable = true;
         support32Bit = true;
+        extraModules = [ pkgs.pulseaudio-modules-bt ];
         package = pkgs.pulseaudioFull;
       };
       bluetooth = {
