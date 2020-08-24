592df1cbc533dea2dcaea7e8b4b968d5844a6c4b
Alex Rice <alexrice999@hotmail.co.uk>
2020-08-24 10:53:16 +0000

    systemd unit for rpi

--- dotfiles/rpi.nix
+++ dotfiles/rpi.nix
@@ -6,6 +6,16 @@
   ];
 
   config = {
+    nixpkgs.overlays = [
+      (self: super: {
+        python37 = super.python37.override {
+	  packageOverrides = pself: psuper: {
+	    gspread = (import ../nixpkgs { }).python37Packages.gspread;
+	  };
+	};
+      })
+    ];
+    
     boot.loader.grub.enable = false;
     boot.loader.generic-extlinux-compatible.enable = true;
 
@@ -29,12 +39,25 @@
     environment.systemPackages = with pkgs; [
       git
       emacs
+      (python37.withPackages (p: [ p.discordpy p.gspread p.oauth2client ]))
     ];
 
     programs.gnupg.agent = {
       enable = true;
       enableSSHSupport = true;
       pinentryFlavor = null;
+    };
+
+    systemd.user.services.bookbot = {
+      enable = true;
+      wantedBy = [ "multi-user.target" ];
+      description = "BookBot service";
+      path = [ (pkgs.python37.withPackages (p: [ p.discordpy p.gspread p.oauth2client ])) ];
+      script = "/home/alex/BookBot/discordbot.py";
+      serviceConfig = {
+        RestartSec = 3;
+        Restart = "always";
+      };
     };
   };
 }
