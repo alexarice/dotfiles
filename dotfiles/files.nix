cf5d292845094b7ffc4ed2e80aee80ac00fde4dd
Alex Rice <alexrice999@hotmail.co.uk>
2021-01-21 12:23:38 +0000

    Use files for zathurarc

--- dotfiles/files.nix
+++ dotfiles/files.nix
@@ -7,5 +7,6 @@ in
   home.file.".ghc/ghci.conf".source = "${dots}/ghci.conf";
   home.file.".config/waybar/config".source = "${dots}/waybar";
   home.file.".config/waybar/style.css".source = "${dots}/waybar.css";
+  home.file.".config/zathura/zathurarc".source = "${dots}/zathurarc";
   home.sessionPath = [ "~/.cabal/bin" ];
 }
