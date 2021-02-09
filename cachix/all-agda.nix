5d1f229a2a3f2a5efc50b46bc7aa646e807d7a11
Alex Rice <alexrice999@hotmail.co.uk>
2021-02-09 11:04:50 +0000

    Add all-agda cachix

---
+++ cachix/all-agda.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://all-agda.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "all-agda.cachix.org-1:yT98x0PL9vS0ZzeR/TXCsb03WuI0ihXAEM6avkNF038="
+    ];
+  };
+}
+    
