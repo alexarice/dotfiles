c6d3f9e79f0345037d49f0c0ba5d9fec0fe2d904
Alex Rice <alexrice999@hotmail.co.uk>
2022-12-06 10:17:28 +0000

    Remove unneeded cache

--- common.nix
+++ common.nix
@@ -19,14 +19,6 @@ with lib;
       settings = {
         trusted-users = [ "root" "alex" ];
 
-        substituters = [
-          "https://cache.iog.io"
-        ];
-
-        trusted-public-keys = [
-          "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-        ];
-
         auto-optimise-store = true;
       };
 
