7c1263fd2434a4259c2a18a9489171ddc8c114d1
Alex Rice <alexrice999@hotmail.co.uk>
2025-10-28 08:41:56 +0000

    Update lock

--- git.nix
+++ git.nix
@@ -2,15 +2,17 @@
   programs = {
     git = {
       enable = true;
-      userName = "Alex Rice";
-      userEmail = "alexrice999@hotmail.co.uk";
       ignores = ["*~"];
       signing = {
         key = "4B9FC04B9EE7F4AE99A7573493DDCD7A2B3F3B88";
         signByDefault = true;
       };
       lfs.enable = true;
-      extraConfig = {
+      settings = {
+        user = {
+          name = "Alex Rice";
+          email = "alexrice999@hotmail.co.uk";
+        };
         core.filemode = false;
         pull.rebase = true;
         credential.helper = "store";
