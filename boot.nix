45d0400751e0da6a56636d79a937ccdc0248ce33
Alex Rice <alexrice999@hotmail.co.uk>
2026-06-14 09:10:36 +0100

    Remove windows entry

--- boot.nix
+++ boot.nix
@@ -15,16 +15,6 @@
     systemd-boot = {
       enable = true;
       consoleMode = "auto";
-      extraEntries = lib.mkIf (config.machine == "desktop") {
-        "windows.conf" = ''
-          title Windows
-          efi /EFI/Microsoft/Boot/bootmgfw.efi
-          sort-key b_windows
-        '';
-      };
-      extraInstallCommands = ''
-        echo "auto-entries no" >> /boot/loader/loader.conf
-      '';
     };
     efi.canTouchEfiVariables = true;
   };
